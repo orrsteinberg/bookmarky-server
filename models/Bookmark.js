bf6f0afc9138f081a01f38fd3cbfc0e2b5c1f8b1
Orr Steinberg <orr.sun10@gmail.com>
2020-12-20 03:31:54 +0200

    Add basic Bookmark model

---
+++ models/Bookmark.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+
+const bookmarkSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    trim: true,
+    minlength: [10, "Bookmark title must be at least 10 characters"],
+    maxlength: [120, "Bookmark title must be less than 120 characters"],
+  },
+  // user: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: "User"
+  //   required: [true, "User ID is required"]
+  // }
+  url: {
+    type: String,
+    required: [true, "URL is required"],
+    trim: true,
+    validate: {
+      validator: (urlString) => {
+        return Boolean(new URL(urlString));
+      },
+    },
+  },
+  description: {
+    type: String,
+    maxlength: [2000, "Bookmark description must be less than 2000 characters"],
+    trim: true,
+  },
+  date: {
+    type: Date,
+    required: [true, "Date is required"],
+  },
+  likes: {
+    // Array of user IDs who liked the bookmark
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+      },
+    ],
+  },
+  likesCount: {
+    type: Number,
+    default: 0,
+  },
+});
+
+// Capitalize title before saving
+bookmarkSchema.pre("save", function () {
+  this.title = this.title.charAt(0).toUpperCase() + this.title.slice(1);
+});
+
+// Cleanup JSON response with .toJSON() method
+bookmarkSchema.set("toJSON", {
+  transform: (_document, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  },
+});
+
+module.exports = mongoose.model("Bookmark", bookmarkSchema);
